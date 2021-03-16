66a8f98200b7a6c1571b1071fbb8658313020265
SelvinSelbaraju <selvinoselbaraju@gmail.com>
2021-03-16 07:06:55 +0000

    basic particle function

--- src/components/basic/SimpleSpring.js
+++ src/components/basic/SimpleSpring.js
@@ -2,26 +2,26 @@ import React, { useRef, useState } from 'react'
 import { useSpring, animated} from 'react-spring';
 
 function SimpleSpring() {
-    const defaultValue = "5";
+    const defaultValues = {mass: 1, tension: 170, friction: 26}
     const [run, setRun] = useState(false)
-    const [mass, setMass] = useState(defaultValue)
-    const [friction, setFriction] = useState(1)
-    const [tension, setTension] = useState(defaultValue)
-    const props = useSpring({config: {friction: friction, mass: mass, tension: tension}, transform: run ? "translate(0%, 100%)" : "translate(0%, 0%)"});
+    const [mass, setMass] = useState(defaultValues.mass)
+    const [friction, setFriction] = useState(defaultValues.friction)
+    const [tension, setTension] = useState(defaultValues.tension)
+    const props = useSpring({reset: !run, config: {friction: friction, mass: mass, tension: tension}, x: run ? 1 : 0});
     return (
         <div className="container">
             <div className="row">
-                <animated.div className="bounce-particle" style={run? props : props.from}>
+                <animated.div className="bounce-particle" style={{config: props.config, transform: run ? props.x.interpolate({range: [0, 0.5, 1], output: ["0%", "100%", "0%"]}).interpolate(x => `translate(0%, ${x})`) : 'translate(0%, 0%'}}>
                 </animated.div>
                 <p className="col">A simple bouncing particle</p>
                 <button onClick={() => setRun(!run)} className="btn btn-primary col-2" type="button">{run ? 'Reset' : 'Start'}</button>
                 <div className="config-params">
                     <label for="mass">Mass: {mass}</label>
-                    <input onChange={(e) => setMass(`${e.target.value}`)} name="mass" type="range" min="1" max="500" defaultValue={defaultValue} />
+                    <input onChange={(e) => setMass(`${e.target.value}`)} name="mass" type="range" min="1" max="500" defaultValue={mass} />
                     <label for="friction">Friction: {friction}</label>
-                    <input onChange={(e) => setFriction(`${e.target.value}`)} name="friction" type="range" min="1" max="10" defaultValue={1} />
+                    <input onChange={(e) => setFriction(`${e.target.value}`)} name="friction" type="range" min="5" max="500" defaultValue={friction} />
                     <label for="tension">Tension: {tension}</label>
-                    <input onChange={(e) => setTension(`${e.target.value}`)} name="tension" type="range" min="1" max="500" defaultValue={defaultValue} />  
+                    <input onChange={(e) => setTension(`${e.target.value}`)} name="tension" type="range" min="5" max="500" defaultValue={tension} />  
 
                 </div>
             </div>
